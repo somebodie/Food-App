5f48824fb4940ef89ef22e14033583ad9b9767fd
alan <alwat91@uga.edu>
2017-02-10 12:10:48 -0500

    Post route hooked up w/ authorize

--- controllers/meals.js
+++ controllers/meals.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router({
     mergeParams: true
 });
+var auth = require('../helpers/auth.js');
 
 router.get('/', function(req, res) {
     res.send('Meals controller working');
 });
 
+router.post('/', auth.authorize, function(req, res) {
+    res.send("post hit");
+});
 
 module.exports = router;
