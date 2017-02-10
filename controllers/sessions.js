d4cfdf9de90ac8e681a9e27514560bd55ffe93ed
alan <alwat91@uga.edu>
2017-02-10 10:40:22 -0500

    user login and logout backend functional

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var auth = require('../helpers/auth.js')
+var User = require('../models/user.js');
 
-router.get('/', function(req, res){
-  res.send('Sessions controller working');
+router.post('/login', auth.loginUser, function(req, res) {
+    console.log(req.session.currentUser);
+    res.json({
+        status: 200,
+        data: req.session.currentUser
+    });
+});
+
+router.delete('/', function(req, res) {
+    req.session.destroy(function() {
+        res.json({
+            status: 204,
+            message: 'destroyed'
+        });
+    });
 });
 
 
