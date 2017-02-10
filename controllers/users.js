019ac03456c64f768233c8bb9291effb01d0d1b8
alan <alwat91@uga.edu>
2017-02-10 08:59:46 -0500

    User post route working with no auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var auth = require('../helpers/auth.js')
+var User = require('../models/user.js');
 
-router.get('/', function(req, res){
-  res.send('Users controller working');
+router.post('/', function(req, res){
+  var user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password_digest: req.body.hashedPassword
+  });
+  console.log(user);
 });
 
 
