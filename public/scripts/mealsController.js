2029a34b55e361dbc4fd85af9abea46fdafda4de
alan <alwat91@uga.edu>
2017-02-14 09:26:31 -0500

    Get week meals functioning

--- public/scripts/mealsController.js
+++ public/scripts/mealsController.js
@@ -4,9 +4,15 @@ angular.module('foodApp')
 
 function mealsController($http, $state, $scope) {
     var self = this;
+
     function getMeals(){
       console.log($scope.currentUser);
+      $http.get(`/users/${$scope.currentUser._id}/meals`)
+      .then(function(response){
+        self.meals = response.recipes;
+      })
     };
-
     getMeals();
+
+
 }
